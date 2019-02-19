882d8cc75c313261dfc715147b5cbb0f4e78cf2c
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2019-02-19 19:26:12 +0300

    changed to upload theme only

--- upload-to-statusproject-net.sh
+++ upload-to-statusproject-net.sh
@@ -1,35 +1,79 @@
 #!/bin/bash
 
-L_PLUGINS_DIR='www/statusproject/public_html/'
-R_PLUGINS_DIR='/public_html/'
+ask() {
+    # https://gist.github.com/davejamesmiller/1965569
+    local prompt default reply
 
-L_SRP_THEME_DIR='www/statusproject/public_html/'
-R_SRP_THEME_DIR='/public_html/'
+    if [ "${2:-}" = "Y" ]; then
+        prompt="Y/n"
+        default=Y
+    elif [ "${2:-}" = "N" ]; then
+        prompt="y/N"
+        default=N
+    else
+        prompt="y/n"
+        default=
+    fi
 
-echo "uploading plugins from $L_PLUGINS_DIR to $R_PLUGINS_DIR ..."
+    while true; do
 
-lftp <<EOF
-set ftp:list-options -a
-set cmd:fail-exit true
-open 103157_haz38_do@ftp01.binero.se
-cd $R_PLUGINS_DIR
-lcd $L_PLUGINS_DIR
-mirror --delete --exclude logs/ --exclude tmp/ --exclude-glob .git
-quit
-EOF
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "$1 [$prompt] "
 
-echo "done"
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read reply </dev/tty
 
-echo "uploading SRP theme from $L_SRP_THEME_DIR to $R_SRP_THEME_DIR .."
+        # Default?
+        if [ -z "$reply" ]; then
+            reply=$default
+        fi
 
-lftp <<EOF
-set ftp:list-options -a
-set cmd:fail-exit true
-open 103157_haz38_do@ftp01.binero.se
-cd $R_SRP_THEME_DIR
-lcd $L_SRP_THEME_DIR
-mirror --delete --exclude logs/ --exclude tmp/ --exclude-glob .git
-quit
-EOF
+        # Check if the reply is valid
+        case "$reply" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
 
-echo "done"
+    done
+}
+
+L_PLUGINS_DIR='/srv/www/statusproject/public_html/wp-content/plugins/'
+R_PLUGINS_DIR='/public_html/wp-content/plugins/'
+
+L_SRP_THEME_DIR='/srv/www/statusproject/public_html/wp-content/themes/statusproject/'
+R_SRP_THEME_DIR='/public_html/wp-content/themes/statusproject/'
+
+L_UPLOADS_DIR='/srv/statusproject-uploads'
+R_UPLOADS_DIR='/public_html/wp-content/uploads/'
+
+USER='103157_haz38_do'
+HOST='ftp01.binero.se'
+read -s -p "Enter password for $USER@$HOST: " PASSWORD
+
+echo " "
+echo "starting..."
+
+
+cd $L_SRP_THEME_DIR \
+    && echo "building theme" \
+    && npm run build \
+    && echo "done" \
+    && echo "uploading SRP theme from $L_SRP_THEME_DIR to $R_SRP_THEME_DIR .." \
+    && lftp -e "mirror -R --exclude .git/ --exclude node_modules/ --exclude tmp/ $L_SRP_THEME_DIR $R_SRP_THEME_DIR; bye;" -u $USER,"$PASSWORD" $HOST \
+    && echo "done"
+
+# if ask "upload plugins?"; then
+#     echo "uploading plugins from $L_PLUGINS_DIR to $R_PLUGINS_DIR ..."
+
+#     lftp -e "mirror -R --exclude logs/ --exclude tmp/ --exclude-glob .git $L_PLUGINS_DIR $R_PLUGINS_DIR; bye;" -u $USER,"$PASSWORD" $HOST
+
+#     echo "done"
+# fi
+
+# if ask "upload uploads dir?"; then
+#     echo "uploading uploads from $L_UPLOADS_DIR to $R_UPLOADS_DIR ..."
+
+#     lftp -e "mirror -R -O $R_UPLOADS_DIR --exclude logs/ --exclude tmp/ --exclude-glob .git -F $L_UPLOADS_DIR/*; bye;" -u $USER,"$PASSWORD" $HOST
+
+#     echo "done"
+# fi
